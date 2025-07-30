2c0a0148d179532c5fe85add4ae10495e5946028
nehit12 <nseversays@gmail.com>
2025-07-30 11:00:52 +0530

    Update index.js

--- index.js
+++ index.js
@@ -41,6 +41,8 @@ app.get('/auth/google/callback',
   })
 );
 
-app.listen(3000, () => {
-  console.log('Server is running on port 3000');
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
+
